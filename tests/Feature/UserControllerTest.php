38b5a53b458844378afbcb2821693564ff916863
Reginaldo <reginaldo.matias@gmail.com>
2024-07-24 10:03:50 -0300

    refactor: change code for improved readability and maintainability

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,181 @@
+<?php 
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function authenticatedUser()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api');
+        return $user;
+    }
+    
+    public function it_can_create_a_user()
+    {
+        $this->authenticatedUser();
+        $userData = User::factory()->make()->toArray();
+
+        $response = $this->postJson('/api/users', $userData);
+
+        $response->assertStatus(201)
+                 ->assertJson([
+                    'name' => $userData['name'],
+                    'email' => $userData['email'],
+                    'password' => $userData['password'];
+                 ]);
+    
+        $this->assertDatabaseHas('users', ['email' => $userData['email']]);
+    }
+
+    public function it_can_update_a_user() 
+    {
+        $this->authenticatedUser();
+        $user = User::factory()->create();
+
+        $updateData = [
+            'name' => 'Test name',
+            'email' => 'test@hotmail.com',
+            'password' => '123456a'
+        ];
+
+        $response = $this->putJson("/api/users/{$user->id}", $updateData);
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                    'name' => 'Test name',
+                    'email' => 'test@hotmail.com',
+                    'password' => '123456a'
+                 ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => 'Test name',
+            'email' => 'test@hotmail.com',
+            'password' => '123456a'
+        ]);
+    }
+
+    public function it_can_list_all_users()
+    {
+        $this->authenticatedUser();
+        User::factory()->count(5)->create();
+
+        $response = $this->getJson('/api/users');
+
+        $response->assertStatus(200)
+                 ->assertJsonCount(5);
+    }
+
+    public function it_can_view_a_single_user()
+    {
+        $this->authenticatedUser();
+        $user = User::factory()->create();
+
+        $response = $this->getJson("/api/users{$user->id}");
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email
+                 ]);
+    }
+
+    public function it_can_delete_a_user()
+    {
+        $this->authenticatedUser();
+        $user = User::factory()->create();
+
+        $response = $this->deleteJson("/api/users/{$user->id}");
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('users', ['id' => $user->id]);
+    }
+
+    public function it_cannot_list_all_users_when_unauthenticated()
+    {
+        User::factory()->count(5)->create();
+
+        $response = $this->getJson('/api/users');
+
+        $response->assertStatus(401);
+
+    }
+
+    public function it_cannot_view_a_single_user_when_unauthenticated()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->getJson("/api/users/{$user->id}");
+
+        $response->assertStatus(401);
+    }
+
+    public function it_cannot_create_a_user_when_unauthenticated()
+    {
+        $userData = User::factory()->make()->toArray();
+
+        $response = $this->postJson('/api/users', $userData);
+
+        $response->assertStatus(401);
+    }
+
+    public function it_cannot_update_a_user_when_unauthenticated()
+    {
+        $user = User::factory()->create();
+
+        $updateData = [
+            'name' => 'Test Name',
+            'email' => 'test@hotmail.com',
+        ];
+
+        $response = $this->putJson("/api/users/{$user->id}", $updateData);
+
+        $response->assertStatus(401);
+    }
+
+    public function it_cannot_delete_a_user_when_unauthenticated()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->deleteJson("/api/users/{$user->id}");
+
+        $response->assertStatus(401);
+    }
+
+    public function it_cannot_update_a_user_when_forbidden()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api'); 
+        $anotherUser = User::factory()->create(); 
+
+        $updateData = [
+            'name' => 'Test Name 2',
+            'email' => 'test2@hotmail.com',
+        ];
+
+        $response = $this->putJson("/api/users/{$anotherUser->id}", $updateData);
+
+        $response->assertStatus(403);
+    }
+
+    public function it_cannot_delete_a_user_when_forbidden()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user, 'api'); 
+        $anotherUser = User::factory()->create(); 
+
+        $response = $this->deleteJson("/api/users/{$anotherUser->id}");
+
+        $response->assertStatus(403);
+    }
+
+
+}
