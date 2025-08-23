f5a8d397991ad019b6c34e6d036e6fed1c8c7454
SaorCampos <saor93@gmail.com>
2025-08-23 14:33:45 -0300

    fix: swagger   api documentation

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -153,52 +153,14 @@ return [
         */
         'securityDefinitions' => [
             'securitySchemes' => [
-                /*
-                 * Examples of Security schemes
-                */
-                /*
-                'api_key_security_example' => [ // Unique name of security
-                    'type' => 'apiKey', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-                    'description' => 'A short description for security scheme',
-                    'name' => 'api_key', // The name of the header or query parameter to be used.
-                    'in' => 'header', // The location of the API key. Valid values are "query" or "header".
-                ],
-                'oauth2_security_example' => [ // Unique name of security
-                    'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-                    'description' => 'A short description for oauth2 security scheme.',
-                    'flow' => 'implicit', // The flow used by the OAuth2 security scheme. Valid values are "implicit", "password", "application" or "accessCode".
-                    'authorizationUrl' => 'http://example.com/auth', // The authorization URL to be used for (implicit/accessCode)
-                    //'tokenUrl' => 'http://example.com/auth' // The authorization URL to be used for (password/application/accessCode)
-                    'scopes' => [
-                        'read:projects' => 'read your projects',
-                        'write:projects' => 'modify projects in your account',
-                    ]
-                ],
-                */
-
                 'bearerAuth' => [ // Unique name of security
-                    'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
+                    'type' => 'http', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
                     'description' => 'Laravel passport oauth2 security.',
                     'in' => 'header',
-                    'scheme' => 'https',
-                    'flows' => [
-                        'password' => [
-                            'authorizationUrl' => config('app.url').'/oauth/authorize',
-                            'tokenUrl' => config('app.url').'/oauth/token',
-                            'refreshUrl' => config('app.url').'/token/refresh',
-                            'scopes' => [],
-                        ],
-                    ],
+                    'scheme' => 'bearer',
+                    'bearerFormat' => 'JWT',
+                    'description' => 'Insira o token no formato: Bearer {seu_token}',
                 ],
-
-                /*
-                'sanctum' => [ // Unique name of security
-                    'type' => 'apiKey', // Valid values are "basic", "apiKey" or "oauth2".
-                    'description' => 'Enter token in format (Bearer <token>)',
-                    'name' => 'Authorization', // The name of the header or query parameter to be used.
-                    'in' => 'header', // The location of the API key. Valid values are "query" or "header".
-                ],
-                */
             ],
             'security' => [
                 /*
@@ -206,12 +168,6 @@ return [
                 */
                 [
                     'bearerAuth' => [],
-
-                    /*
-                    'oauth2_security_example' => [
-                        'read',
-                        'write'
-                    ],*/
                 ],
             ],
         ],
