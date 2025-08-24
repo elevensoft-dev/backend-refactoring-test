174a0e4a7fd1ffcf1974fbefa0cfcae660ca7b84
Rafael Ferreira Gomes <faelfg@gmail.com>
2025-08-24 14:45:14 -0300

    refactor: change code for improved readability and maintainability

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,12 @@ use Illuminate\Support\Facades\Route;
 */
 //Route::apiResource('users', UserController::class);
 
-Route::group(['prefix'=> 'users'], function (): void {
-    Route::get('/', AllUsersController::class);
-    Route::post('/', CreateNewUserController::class);
-    Route::get('/{id}', UserByIdController::class);
-    Route::patch('/{id}', UpdateUserController::class);
-    Route::delete('/{id}', DeleteUserController::class);
+Route::group(['middleware' => '', 'prefix'=> ''], function (): void {
+    Route::group(['prefix'=> 'users'], function (): void {
+        Route::get('/', AllUsersController::class);
+        Route::post('/', CreateNewUserController::class);
+        Route::get('/{id}', UserByIdController::class);
+        Route::patch('/{id}', UpdateUserController::class);
+        Route::delete('/{id}', DeleteUserController::class);
+    });
 });
