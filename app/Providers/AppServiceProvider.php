15dc0bae8eba2e19b916e8035e87f6f1f797ad07
Walter Discher Cechinel <mistrim@gmail.com>
2025-09-06 16:03:04 -0300

    fix: configure scramble

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Models\User;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +26,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->isLocal()) {
+            Scramble::configure()
+                ->withDocumentTransformers(function (OpenApi $openApi) {
+                    $openApi->secure(
+                        SecurityScheme::http('bearer'),
+                    );
+                });
+        }
+
         ResetPassword::createUrlUsing(function (User $user, string $token) {
             $email = urlencode($user->email);
 
