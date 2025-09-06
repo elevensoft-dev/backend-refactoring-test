2e2ba431a1869d74a65a24bbf22105155693d7a7
Walter Discher Cechinel <mistrim@gmail.com>
2025-09-06 13:55:24 -0300

    refactor: delegate reset password url to frontend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (User $user, string $token) {
+            $email = urlencode($user->email);
+
+            if ($user instanceof User) {
+                return config('app.frontend_url')."/reset-password?token={$token}&email={$email}";
+            }
+        });
     }
 }
